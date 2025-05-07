6da722aca26c1455f63b51f5ae5027ca93eb28e1
Zehab-bid <zehabidha152gmail.com>
2025-05-07 14:23:58 +0700

    proges 8

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -80,3 +80,19 @@ void binarySearch()
         int high = nPanjang - 1;
 
         do
+        {
+            int mid = (low + high) / 2;
+            if (element[mid] == x)
+            {
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << ".\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
